1a4c360479c7eac72efef72cb06cbb1f77122bfb
jason <jbut@swin.edu.au>
2026-01-25 13:25:06 +1100

    Type hinting completion and code/comment clean-up for final version before first real release

--- zpool_monitor/zpool/vdev.py
+++ zpool_monitor/zpool/vdev.py
@@ -5,6 +5,8 @@ can then be accessed as a list of data to insert into table cells for display
 
 # Import System Libraries
 from datetime import datetime
+from typing import Any
+from rich.console import RenderableType
 from rich.padding import Padding
 
 # Import zpool.formatting functions
@@ -13,11 +15,11 @@ from . import humanise, warning_colour_number, create_progress_renderable
 
 class VDEV:
     """
-    Extracts information for a single VDEV as returned by 'zpool status' and provides relevant data as a list for display
+    Extracts information for a single VDEV as returned by 'zpool status' and converts into Rich Renderables for display as a table with other VDEV instances
     """
     state_colours: dict[str, str] = {'ONLINE': '[green]', 'OFFLINE': '[bold orange3]', 'DEGRADED': '[bold orange3]'}
 
-    def __init__(self, vdev_data: dict, depth: int):
+    def __init__(self, vdev_data: dict[str, Any], depth: int):
         """
         Construct instance of class to map status for a single VDEV
 
@@ -29,51 +31,46 @@ class VDEV:
         # Extract VDEV size - done here as this is a number not a string
         vdev_size = vdev_data.get('phys_space', vdev_data.get('def_space', 0))
 
-        # Table row consists of:
-        # 1) VDEV name indented to represent depth. Name is coloured based on VDEV state
-        # 2) If the VDEV has a size, the humanised display of size, otherwise an empty string
-        # 3) VDEV state coloured based on current state
-        # 4) VDEV device ID, if this doesn't exist, then the VDEV path. If neither exist, empty string
-        # 5, 6, 7) Read/Write/Checksum error, coloured as a warning
-        # 8) If the VDEV is trimmable, the current trim state (as returned by __parse_trim_state(), otherwise an empty string
-        self.__data = {'Device Name': Padding(f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['name']}', (0, 0, 0, depth * 2)),
-                       'Size': humanise(vdev_size) if vdev_size > 0 else '',
-                       'State': f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['state']}',
-                       'Device': vdev_data.get('devid', vdev_data.get('path', '')),
-                       'Read': warning_colour_number(vdev_data['read_errors']),
-                       'Write': warning_colour_number(vdev_data['write_errors']),
-                       'Checksum': warning_colour_number(vdev_data['checksum_errors']),
-                       'Last Trim': self.__parse_trim_state(vdev_data) if 'trim_notsup' in vdev_data else ''
-                       }
-        self.__row_data = [Padding(f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['name']}', (0, 0, 0, depth * 2)),
-                           humanise(vdev_size) if vdev_size > 0 else '',
-                           f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['state']}',
-                           vdev_data.get('devid', vdev_data.get('path', '')),
-                           warning_colour_number(vdev_data['read_errors']),
-                           warning_colour_number(vdev_data['write_errors']),
-                           warning_colour_number(vdev_data['checksum_errors']),
-                           self.__parse_trim_state(vdev_data) if 'trim_notsup' in vdev_data else ''
-                           ]
+        # Extract information into dictionary mapping column headers to data as a Rich Renderable, column order is the key order listed here, special cases:
+        #   - VDEV name indented to represent depth. Name and state is coloured based on VDEV state
+        #   - Trim renderable calculated by __parse_trim_state() method due to multiple possibilities
+        self.__data: dict[str, RenderableType] = {'Device Name': Padding(f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['name']}', (0, 0, 0, depth * 2)),
+                                                  'Size': humanise(vdev_size) if vdev_size > 0 else '',
+                                                  'State': f'{VDEV.state_colours.get(vdev_data['state'], '[bold red]')}{vdev_data['state']}',
+                                                  'Device': vdev_data.get('devid', vdev_data.get('path', '')),
+                                                  'Read': warning_colour_number(vdev_data['read_errors']),
+                                                  'Write': warning_colour_number(vdev_data['write_errors']),
+                                                  'Checksum': warning_colour_number(vdev_data['checksum_errors']),
+                                                  'Last Trim': self.__parse_trim_state(vdev_data)
+                                                  }
 
-    def __parse_trim_state(self, vdev_data: dict):
+    def __parse_trim_state(self, vdev_data: dict[str, Any]) -> RenderableType:
         """
-        Parse trim state variables for VDEV as output by 'zpool status' and generate a rich renderable to display status
+        Parse trim state variables for VDEV as output by 'zpool status' and generate a rich renderable to display trim status
 
         :param vdev_data: JSON output for single VDEV from 'zpool status' mapped to a dictionary
 
         :return: Rich renderable to display current trim status
         """
+        # VDEV is not a real device, return en empty string
+        if 'trim_notsup' not in vdev_data: return ''
+
         match vdev_data['trim_notsup']:
             case 0:
                 # Trim supported on this VDEV
                 match vdev_data['trim_state']:
+                    case 'UNTRIMMED':
+                        # Never trimmed, return string indicating this
+                        return f'❌ Never been trimmed'
                     case 'COMPLETE':
                         # Trim not running, return time of last trim as a string
                         return datetime.fromtimestamp(vdev_data['trim_time']).strftime('%c')
                     case 'ACTIVE':
                         # Trim running, create and return a rich Progress Bar displaying trim progress
                         complete = 100 * vdev_data['trimmed'] / vdev_data['to_trim']
-                        return create_progress_renderable(f'✂️ {humanise(vdev_data['trimmed'])} of {humanise(vdev_data['to_trim'])}', '', complete)
+                        return create_progress_renderable(pre_bar_txt=f'✂️ {humanise(vdev_data['trimmed'])} of {humanise(vdev_data['to_trim'])}',
+                                                          post_bar_txt='',
+                                                          percentage=complete)
                     case '_':
                         # Invalid value for trim state, should not get here
                         raise ValueError(f'Trim state ({vdev_data['trim_state']}) returned by \'zpool status\' is invalid')
@@ -86,12 +83,14 @@ class VDEV:
                 # Invalid value for trim state, should not get here
                 raise ValueError(f'Unexpected value (trim_nosup={vdev_data['trim_notsup']}) returned by \'zpool status\'')
 
+        raise ValueError(f'Unexpected error parsing trim state')
+
     @property
-    def label_data(self) -> list:
+    def label_data(self) -> list[str]:
         """Return a list containing VDEV column labels to set up the header of a table for display"""
         return list(self.__data.keys())
 
     @property
-    def row_data(self) -> list:
-        """Return a list containing VDEV data to populate a single row of a table for display"""
+    def row_data(self) -> list[RenderableType]:
+        """Return a list (as a table row) containing Rich renderables for each column label for display"""
         return list(self.__data.values())
